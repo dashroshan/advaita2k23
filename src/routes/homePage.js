7d62e9319d185d3d0691ae20f1aabffbcefbb064
Roshan Dash <roshan1337d@gmail.com>
2023-02-09 02:01:54 +0530

    check

--- src/routes/homePage.js
+++ src/routes/homePage.js
@@ -1,11 +1,22 @@
 import classes from "./homePage.module.css";
-import heroImg from "../assets/hero.png"
+import iconIg from "../assets/instagram.svg";
+import iconTw from "../assets/twitter.svg";
+import iconYt from "../assets/youtube.svg";
 
 export default function HomePage() {
     return (
         <div className={classes.homePage}>
-            <section className={classes.hero}>
+            <section id="herosection" className={classes.hero}>
+                <div className={classes.leftVertical}>
+                    <div>FOLLOW US ON: </div>
+                    <a href="#"><img src={iconIg} alt="social" /></a>
+                    <a href="#"><img src={iconTw} alt="social" /></a>
+                    <a href="#"><img src={iconYt} alt="social" /></a>
+                </div>
 
+                <div className={classes.rightVertical}>
+                    IIIT BHUBANESWAR, ODISHA
+                </div>
             </section>
         </div>
     );
