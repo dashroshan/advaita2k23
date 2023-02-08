5809dc89d9d139f176c35af990deb3c122e8a016
Roshan Dash <roshan1337d@gmail.com>
2023-02-09 00:06:26 +0530

    nav 2

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,43 +5,24 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useState, useEffect } from "react";
 
 export default function NavBar() {
-    const [menuOpen, setMenuOpen] = useState(true);
-    const container = {
-        hidden: { opacity: 1, scale: 0 },
-        visible: {
-            opacity: 1,
-            scale: 1,
-            transition: {
-                delayChildren: 0.3,
-                staggerChildren: 0.1,
-            }
-        }
-    };
-
-    const item = {
-        hidden: { opacity: 0 },
-        visible: {
-            opacity: 1
-        }
-    };
-
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
             <AnimatePresence>
                 {menuOpen ?
                     <motion.div
-                        initial="hidden"
-                        animate="visible"
-                        variants={container}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
                         transition={{ duration: 0.5 }}
                         className={classes.overlay} onClick={() => setMenuOpen(!menuOpen)}
                     >
-                        <motion.div key={1} variants={item}>EVENTS</motion.div>
-                        <motion.div key={2} variants={item}>PRONITES</motion.div>
-                        <motion.div key={3} variants={item}>SPONSORS</motion.div>
-                        <motion.div key={4} variants={item}>CONTACTS</motion.div>
-                        <motion.div key={5} variants={item}>REGISTER</motion.div>
+                        <div>EVENTS</div>
+                        <div>PRONITES</div>
+                        <div>SPONSORS</div>
+                        <div>CONTACTS</div>
+                        <div>REGISTER</div>
                     </motion.div>
                     :
                     <motion.nav
