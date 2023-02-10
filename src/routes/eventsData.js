1b8c5c38d948edcff800861e00ac3ecc5317d074
Roshan Dash <roshan1337d@gmail.com>
2023-02-11 03:23:13 +0530

    tech events

--- src/routes/eventsData.js
+++ src/routes/eventsData.js
@@ -15,73 +15,73 @@ export const eventsData = [
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Who doesn't love car races- a tournaments where the drivers showcase their skills and their cars as they race against each other through multiple obstacles and difficult tracks. Sounds amazing doesn't it? Well, you can be a part of it too. We bring to you the \"Dirt Rush\" a form of Remote-Controlled car racing that takes place on dirt tracks where you have to navigate through obstacles such as jumps, berms, and whoops in the least time possible.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/dirt-rush.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "You must be thinking \"Well here's another one of those robo events\", don't be so quick to judge this one is entirely different.Remember the childhood games when we had to follow some lines and patterns while walking.This event helps you relive that very moment of your childhood with a small twist: This time it's not you but your robot \"the line follower robot\" who has to follow the line. The line follower robot is an autonomous mobile machine that can detect and follow a black line or a strip made on the floor within a specified margin of error. The robots compete against time and the one finishing in the least time wins.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/line-follower.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Robots are taking over a lot of our departments. We can sit and relax as they do our work. Signs of concern perhaps, but not in this case. \"Robo race\" is an event where not only your robot but also you have to do the work. The participants have to design a robot either wired or wireless within specified dimensions that operates manually and can traverse through all turns of the track. They race against time and other competitors in an attempt to emerge as the ultimate robo racer.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/robot-race.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "We live in an era where things are replaced by their virtual counterparts, meets have been replaced by virtual meets, letters by WhatsApp texts and so on. We decided to use this flavour and implement it in sports too and this is what lead to \"FPV racing\". FPV, also called first person view drones, are aerial vehicles fitted with cameras that live stream the video to the operator’s headset. The participants have to control these drones as race through a course and compete against each other to determine the best.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/fpv-racing.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "The world around us is evolving rapidly and its best for us to evolve as well. With new tools and technologies surfacing every now and then, it’s a must for any person to know about them. Sounds quite daunting, doesn't it? Well, you won't be alone on this journey. We bring to you, something very different from your daily classroom lectures, a two hour long hands on workshop on the new technologies by tech experts where you can learn: - - open source - cracking best internships - grab more opportunities - insights on college life and further",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/tech-workshop.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Ever went on hunting? Nope it’s not a normal hunt, here you need to hunt down the hackers instead of the animals! This thrilling event is a proper red teaming event where the participants will emulate real life black hat hackers and understand their tactics and techniques.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/cyber-hunt.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Remember the golden old days of science exhibition in school? Well, we’re bringing back that nostalgia through this event! This amazing event is going to be a day long exhibition of the most interesting and useful projects contributed by the students and community.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/not-made.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Can you imagine a Tech-fest without a Hackathon? Obviously not. So, here we bring in our hackathon which is going to be the flagship tech event of Advaita- Hackfest 23, a software and hardware hackathon which is open to all. Hackfest-Advaita will begin with its first round of idea submission from 23rd to 27th Jan on the platform of Devpost. The second round will be held offline at the IIIT-BH campus on 16th and 17th of Feb.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/intra-hackathon.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "This is a cyber security event where the participants will have to compromise servers, PCs, firewalls and some other networking device to get access to sensitive information, data and exfiltrating to some other locations.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/capture-the-flag.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Guidance is the biggest weapon that helps us fight our way through success. How amazing does it feel when experienced Tech leads guide us? Well, this is obviously going to be an insightful seminar or talk related to new technologies by a popular speaker or community person. This is going to be a promising and engaging session for all the tech enthusiasts. So, come on and gain new insights and perspectives on the future of technology by the famous speaker.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/tech-talk.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Ever made a team and worked individually? Well, this event will test your code as well as team compatibility. It is a coding contest with a single coding round for teams made of two. ICPC format of contest will be followed with some different rules. Both the team members will solve the same question but not together, they will solve the question individually and after a time limit, they will switch their positions and continue till the time reaches for that particular question. So, work on the live problem in a disjointed team of two and switch your positions after half the total time.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/not-made.webp"
             },
             {
                 date: "2ND FEB",
                 time: "10:00 AM",
                 description: "Coding is great but penalties are not and moreover when it’s bitter or worse. Couldn’t understand? Well, this fun event will test your skills through guts! It is an interesting fun coding competition which is formatted into two rounds. The first online round will select the top 50 coders who will compete in different groups. Each group will get a single question and failing to complete it within the given decreasing time limit will result in a penalty. The penalty will be in the form of a not-so-good drink. So, code your way out within time to escape the penalty of a dreadful drink.",
-                image: "/event-images/robo-wars.webp"
+                image: "/event-images/swig-n-code.webp"
             },
         ]
     },
