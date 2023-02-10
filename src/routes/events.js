216e1fa917020fc5e3b02351a098a924942cc5e9
Roshan Dash <roshan1337d@gmail.com>
2023-02-10 23:24:05 +0530

    event header

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,12 +1,14 @@
 import classes from "./events.module.css";
-import bgpn from "../assets/bgpn.jpg"
+import bgpn from "../assets/bgpn.jpg";
+import eventsText from "../assets/events.png";
 
 export default function EventsPage(props) {
     return (
-        <div className={classes.pronights}>
+        <div className={classes.eventsPage}>
             <div className={classes.header}>
-                <img src={bgpn} alt="pronights" />
+                <img src={bgpn} alt="pronights" className={classes.bgimgheader} />
             </div>
+            <img src={eventsText} alt="events schedule" className={classes.eventText} />
         </div>
     );
 }
