4aa2b46415eba7a25bce6bcb35c3e510a359893f
Roshan Dash <roshan1337d@gmail.com>
2023-02-12 23:12:54 +0530

    remove binary

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,7 +3,6 @@ import advaitaLogo from "../../assets/logo.webp";
 
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import TopBinary from "./topBinary";
 import { HashLink } from "react-router-hash-link";
 
 export default function NavBar() {
@@ -27,30 +26,23 @@ export default function NavBar() {
                         <div><a style={{ textDecoration: 'inherit', color: 'inherit' }} target="_blank" href="/">REGISTER</a></div>
                     </motion.div>
                     :
-                    <motion.nav
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.5 }}
-                        className={classes.nav}
-                    >
-                        <HashLink style={{ textDecoration: 'inherit', color: 'inherit' }} to="/#"><img src={advaitaLogo} alt="Advaita" /></HashLink >
-                        <div className={classes.btns} onClick={() => setMenuOpen(!menuOpen)}>
-                            <div />
-                            <div />
-                        </div>
-                    </motion.nav>
+                    <div>
+                        <div className={classes.navWrap} />
+                        <motion.nav
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className={classes.nav}
+                        >
+                            <HashLink style={{ textDecoration: 'inherit', color: 'inherit' }} to="/#"><img src={advaitaLogo} alt="Advaita" /></HashLink >
+                            <div className={classes.btns} onClick={() => setMenuOpen(!menuOpen)}>
+                                <div />
+                                <div />
+                            </div>
+                        </motion.nav>
+                    </div>
                 }
-            </AnimatePresence>
-            <AnimatePresence>{menuOpen ? null :
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    <TopBinary />
-                </motion.div>}
             </AnimatePresence>
         </>
     );
