6064523e029245c079462d65654d2e80f53ddc7c
Roshan Dash <roshan1337d@gmail.com>
2023-02-23 00:02:31 +0530

    pop up

--- src/components/popbox/index.js
+++ src/components/popbox/index.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import AngleButton from "../angleButton";
 import classes from "./index.module.css";
+import CountUp from 'react-countup';
 
 export default function PopBox(props) {
     const visTrue = classes.modal + " " + classes.show_modal;
@@ -8,7 +9,11 @@ export default function PopBox(props) {
         <div className={props.popUp ? visTrue : visFalse}>
             <div className={classes.modal_content}>
                 <span className={classes.close_button} onClick={() => props.setPopUp(false)}>x</span>
-                <h1>Hello, I am a modal!</h1>
+                <h2>Please note</h2>
+                <div style={{ opacity: 0.75 }}>If you wish to pay using UPI, please select one of the wallet options like PhonePe in our payment gateway.</div>
+                <div className={classes.buttonWrap}>
+                    <AngleButton text="CONTINUE" />
+                </div>
             </div>
         </div>
     );
